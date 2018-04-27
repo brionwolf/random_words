0d1a9a1a23cbf21082e51f8f74683bb299e76634
Brion Wolf <brionwolf@users.noreply.github.com>
2018-04-26 23:50:42 -0400

    Input text parsed for words, split into array.

--- media/js/main.js
+++ media/js/main.js
@@ -3,7 +3,12 @@ $(document).ready(function() {});
 
 // Randomize Button Functionality
 $("button#randomize").click(function(e){
-  e.preventDefault();
+  e.preventDefault(); // prevent page reload
+  // Collect value from input field, clean it, and place words in an array
   var words_to_randomize = $("input#words_to_randomize").val();
-  console.log(words_to_randomize)
+  words_to_randomize = words_to_randomize.replace(/\s+/g, '');
+  var words_array = words_to_randomize.split(",")
+  // Send Success to Console.
+  console.log("Collected the following \"" + words_array.length + "\" words:");
+  console.log(words_array);
 });
